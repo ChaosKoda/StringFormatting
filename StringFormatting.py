882796e098d576de015e293a98dab532ceeafa3d
chaoskoda <chaoskoda15@gmail.com>
2020-10-20 11:06:28 -0400

    Basic examples of string formatting

---
+++ StringFormatting.py
@@ -0,0 +1,20 @@
+#This script shows different built in 
+#formatting options - written by Kody Noe
+
+
+names = ['Jeff', 'Gary', 'Jill', 'Samantha', ]
+
+
+print('Concatenation example...')
+for name in names:
+	print('Hi, ' + name)
+
+
+print('\nJoin example...')
+for name in names:
+	print(''.join(['Hello, ', name]))
+
+
+print('\nFormatting Example...')
+for name in names:
+	print('Hello, {}'.format(name))
